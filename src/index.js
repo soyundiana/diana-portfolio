69e547ca57b2109982ab584667cbe0132ea972ea
soyundiana <142823980+soyundiana@users.noreply.github.com>
2024-05-17 12:55:48 +0100

    latest updates include: updated screenshots, updated CSS, added javascript

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const div = document.querySelector(".text");
+const text = "Currently seeking new exciting projects with HTML, CSS, and JavaScript.";
+
+function textTypingEffect(element, text, i = 0){
+element.textContent += text[i];
+
+if (i === text.length - 1 ) {
+    return;
+}
+setTimeout(() => textTypingEffect(element, text, i + 1), 50);
+
+}
+textTypingEffect(div, text);
