53c3d832ce885e75a2174babef35755e1f9a461a
soyundiana <142823980+soyundiana@users.noreply.github.com>
2024-06-17 16:53:19 +0100

    updated fonts, updated project and about me placeholders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const div = document.querySelector(".text");
-const text = "Currently seeking new exciting projects with HTML, CSS, and JavaScript.";
+const text = "Currently seeking new exciting projects with HTML, CSS, JavaScript & React";
 
 function textTypingEffect(element, text, i = 0){
 element.textContent += text[i];
